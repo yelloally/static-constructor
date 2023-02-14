1c67ac6226879e697ddbdc5c7909b051a9629ae5
Alona <122108963+yelloally@users.noreply.github.com>
2023-02-14 15:39:32 +0100

    Add files via upload

---
+++ Sttic_Constructor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class DBRepository
+{
+    private static string connectionString;
+
+    static DBRepository()
+    {
+        ConfigurationManager configurationManager = new ConfigurationManager();
+
+        connectionString = configurationManager.GetConnectionString();
+    }
+
+    public void GetData()
+    {
+        Console.WriteLine("Using: " + connectionString);
+    }
+
+    class ConfigurationManager
+    {
+        public string GetConnectionString()
+        {
+            return "local Db";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBRepository dBRepository = new DBRepository();
+
+            dBRepository.GetData();
+        }
+    }
+}
